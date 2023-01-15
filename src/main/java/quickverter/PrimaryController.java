76502670bd05ff892797ddbe57f1086fe8dbe2a7
Aku_02 <105957678+axelmoralesf@users.noreply.github.com>
2023-01-15 10:57:37 -0600

    PrimaryCrontoller
    Remove the files input type and now can convert docx to pdf with aspose

--- src/main/java/quickverter/PrimaryController.java
+++ src/main/java/quickverter/PrimaryController.java
@@ -6,8 +6,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-import com.aspose.words.Document;
-import com.aspose.words.DocumentBuilder;
+import com.aspose.words.*;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -24,8 +23,10 @@ public class PrimaryController implements Initializable{
     @FXML private ComboBox typeFileFinal;
     
     String Nombre;
+    String Tipo;
+    String TipoF;
+    int type;
 
-    ArrayList<String> TiposC = new ArrayList<String>();
     ArrayList<String> TiposaC = new ArrayList<String>();
 
     public void Alarma(){
@@ -36,6 +37,14 @@ public class PrimaryController implements Initializable{
         alert.showAndWait();
     }
 
+    public void AlarmaCarga(){
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Mensaje del Sistema");
+        alert.setHeaderText(null);
+        alert.setContentText("El archivo se cargo con exito");
+        alert.showAndWait();
+    }
+
     @FXML
     private void btnFile_action() throws IOException {
         FileChooser fileChooser = new FileChooser();
@@ -43,19 +52,31 @@ public class PrimaryController implements Initializable{
         File ruta = fileChooser.showOpenDialog(null);
         Nombre = ruta.getAbsolutePath();
         TextName.setText(Nombre);
+        type = Nombre.length();
+        Tipo = Nombre.substring(type-4,type);
+        if(Tipo.charAt(0)=='.'&&Tipo.charAt(1)=='j'&&Tipo.charAt(2)=='p'&&Tipo.charAt(3)=='g'){
+            TipoF="JPG";
+        }
+        if(Tipo.charAt(0)=='d'&&Tipo.charAt(1)=='o'&&Tipo.charAt(2)=='c'&&Tipo.charAt(3)=='x'){
+            TipoF="DOCX";
+        }
+        System.out.println(Tipo);
+        AlarmaCarga();
     }
 
     @FXML
     private void btnConvertir_action() throws Exception {
-        if(typeFile.getSelectionModel().getSelectedItem()==".JPG (Imagen)"&&typeFileFinal.getSelectionModel().getSelectedItem()=="PDF"){
+        if(typeFileFinal.getSelectionModel().getSelectedItem()=="PDF"&&TipoF=="JPG"){
             Document doc = new Document();
             DocumentBuilder builder = new DocumentBuilder(doc);
             builder.insertImage(Nombre);
             doc.save(Nombre+"Output.pdf");
             Alarma();
         }
-        if(typeFile.getSelectionModel().getSelectedItem()==".DOCX (Documento Word)"){
-            System.out.println("DOCX a PDF");
+        if(typeFileFinal.getSelectionModel().getSelectedItem()=="PDF"&&TipoF=="DOCX"){
+            Document doc = new Document(Nombre);
+            doc.save(Nombre+"Output.pdf");
+            Alarma();
         }
         else{
             System.out.println("Seleccione un tipo de archivo valido");
@@ -64,10 +85,7 @@ public class PrimaryController implements Initializable{
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        TiposC.add(".JPG (Imagen)");
-        TiposC.add(".DOCX (Documento Word)");
         TiposaC.add("PDF");
-        typeFile.getItems().addAll(TiposC);
         typeFileFinal.getItems().addAll(TiposaC);
     }
 }
